6830f0dfa785356a6ffff4729d56190f0d22e462
李旭峰 <729200906@qq.com>
2018-04-08 16:37:35 +0800

    test

---
+++ pages/answer/index.js
@@ -0,0 +1,10 @@
+Page({
+    data: {
+        title: 'TITLE',
+        content: 'CONTENT CONTENT CONTENT CONTENT CONTENT CONTENT CONTENT CONTENT ',
+        footer: 'FOOTER'
+    },
+    onLoad(){
+        
+    }
+})
